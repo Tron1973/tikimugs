50e4d43772c7e03cc7244031f65b465075a6e5eb
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-05-26 12:23:30 -0700

    fixed about text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,12 @@ function Home() {
         <div className="home">
           <>
             <h2>A personal collection of tiki mugs</h2>
-            <p>These have been collected over the past 25 years,</p>
-            <p>including authentic midcentury mugs as well as
+            <div className="about">
+            <p>These have been collected over the past 25 years,
+            including authentic midcentury mugs as well as
             modern styles.</p>
-            <p>Click on the categories to check 'em out!</p>  
+            <p>Click on the categories to check 'em out!</p> 
+            </div> 
             <h3 className="comments">Comments/questions:</h3>
             <h4 className="email"><a href="mailto:ryan@ryanwilsondesign.com">ryan@ryanwilsondesign.com</a></h4>
             <h4 className="website-design">
