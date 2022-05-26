9d1c991ae840ff326cfcf038545059fc956d2d50
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-05-26 12:12:39 -0700

    fix home page text width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home() {
         <div className="home">
           <>
             <h2>A personal collection of tiki mugs</h2>
-            <p>These have been collected over the past 25 years,
-            including authentic midcentury mugs as well as
-            modern styles.
-            Click on the categories to check 'em out!</p>  
+            <p>These have been collected over the past 25 years,</p>
+            <p>including authentic midcentury mugs as well as
+            modern styles.</p>
+            <p>Click on the categories to check 'em out!</p>  
             <h3 className="comments">Comments/questions:</h3>
             <h4 className="email"><a href="mailto:ryan@ryanwilsondesign.com">ryan@ryanwilsondesign.com</a></h4>
             <h4 className="website-design">
