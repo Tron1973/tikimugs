190cee3e0a388f5d27bc5f8322ee8cf3434be435
Ryan Wilson <ryanellsworthwilson@gmail.com>
2022-05-26 12:31:26 -0700

    adjust paragraph width in home section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,10 @@ function Home() {
         <div className="home">
           <>
             <h2>A personal collection of tiki mugs</h2>
-            <div className="about">
-            <p>These have been collected over the past 25 years,
-            including authentic midcentury mugs as well as
+            <p>These have been collected over the past 25 years,</p>
+            <p>including authentic midcentury mugs as well as
             modern styles.</p>
-            <p>Click on the categories to check 'em out!</p> 
-            </div> 
+            <p>Click on the categories to check 'em out!</p>  
             <h3 className="comments">Comments/questions:</h3>
             <h4 className="email"><a href="mailto:ryan@ryanwilsondesign.com">ryan@ryanwilsondesign.com</a></h4>
             <h4 className="website-design">
